b8cff1b368bcb4da75e72ab02a493fb83fbc2f16
sidegouth <headlecmarcus@gmail.com>
2025-01-10 17:55:01 +0000

    exercicio 3 versão 2

---
+++ ex3v2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int numero;
+	int numero2;
+	cout << "Digite um número: " << endl;
+	cin >> numero;
+	cout << "Digite outro número: " << endl;
+	cin >> numero2;
+	int soma = numero + numero2;
+	cout << soma << endl;
+	return 0;
+}
