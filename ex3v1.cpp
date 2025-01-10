34a1761269f099ba3e0371cc4fcb2cba8621bb75
sidegouth <headlecmarcus@gmail.com>
2025-01-10 17:52:06 +0000

    exercicio 3 versão 1

---
+++ ex3v1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int numero;
+	int numero2;
+	cout << "Digite um número: " << endl;
+	cin >> numero;
+	cout << "Digite outro número: " << endl;
+	cin >> numero2;
+	cout << numero + numero2 << endl;
+	return 0;
+}
