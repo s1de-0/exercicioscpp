d9f100e0ff63e6f1f3d571baa70c957e6be563ab
sidegouth <headlecmarcus@gmail.com>
2025-01-10 17:47:22 +0000

    exercicio 2

---
+++ ex2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int numero;
+	cout << "Digite um número: " << endl;
+	cin >> numero;
+	cout << "Número digitado: " << numero << endl;
+	return 0;
+}
