92e4dd03e0e25b4d36b2be4855ebb7ed84f6a6a6
sidegouth <headlecmarcus@gmail.com>
2025-01-31 22:19:51 +0000

    colocando o exercicio 4

---
+++ exercicio4/ex4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n1,n2,n3,n4;
+	cout << "Digite as suas 4 notas bimestrais aí, pra eu ver um negócio" << endl;
+	cin >> n1;
+	cin >> n2;
+	cin >> n3;
+	cin >> n4;
+	cout << (n1 + n2 + n3 + n4) / 4 << endl;
+	return 0;
+}
